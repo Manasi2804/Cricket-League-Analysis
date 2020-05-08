4736dc3696f833300c0c492e68a054a28925279d
Manasi2804 <shahmanasi281996@gmail.com>
2020-05-08 14:04:08 +0530

    ADDED:Welcome Message

---
+++ src/main/java/com/bl/cricketanalysis/CricketAnalysis.java
@@ -0,0 +1,7 @@
+package com.bl.cricketanalysis;
+
+public class CricketAnalysis {
+    public static void main(String[] args){
+        System.out.println("Welcome to Cricket League Analysis Problem");
+    }
+}
