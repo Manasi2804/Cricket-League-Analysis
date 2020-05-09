a612663b57bc2838bf2fa3265ba66d5184976253
Manasi2804 <shahmanasi281996@gmail.com>
2020-05-09 20:51:07 +0530

    ADDED:Load the data

--- src/main/java/com/bl/cricketanalysis/CricketAnalysis.java
+++ src/main/java/com/bl/cricketanalysis/CricketAnalysis.java
@@ -1,7 +1,29 @@
 package com.bl.cricketanalysis;
 
-public class CricketAnalysis {
-    public static void main(String[] args){
-        System.out.println("Welcome to Cricket League Analysis Problem");
+import com.bl.cricketanalysis.model.IPLMostRuns;
+import com.opencsv.CSVReader;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
+
+public class CricketAnalysis{
+    public static int loadData(String filePath) throws IOException {
+        int count = 0;
+        try (Reader reader = Files.newBufferedReader(Paths.get(filePath))) {
+            CSVReader csvReader = new CSVReader(reader);
+            CsvToBean<IPLMostRuns> csvToBean = new CsvToBeanBuilder<IPLMostRuns>(csvReader).
+                    withType(IPLMostRuns.class)
+                    .build();
+            Iterator<IPLMostRuns> csvRecords = csvToBean.iterator();
+            while (csvRecords.hasNext()) {
+                count++;
+                csvRecords.next();
+            }
+        }
+        return count;
     }
 }
